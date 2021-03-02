152dca6c025f325c8824e021f42ff788902721a1
Dmitry Borisenko <borisenkodv@gmail.com>
2021-03-02 12:23:13 +0600

    Decorator Pattern Add: exercise

---
+++ src/main/java/com/lineate/bench/pattern/decorator/exercise/Rectangle.java
@@ -0,0 +1,8 @@
+package com.lineate.bench.pattern.decorator.exercise;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
